64932a55bbc66ec8a70580910c9a877393631f20
Tristen <tristen.hammon21@gmail.com>
2022-11-01 15:19:34 -0500

    commit tree 56213b9b6dd5f17622509f8a91eea78b8b946df8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,13 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
 
     post.update(post_params)
-
-    render json: post
+    
+    if post.valid?
+      render json: post
+    else
+      render json: { errors: post.errors }, status: :unprocessable_entity
+    end
+    
   end
 
   private
